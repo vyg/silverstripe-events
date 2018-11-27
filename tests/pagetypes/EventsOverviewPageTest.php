1184a04a3eb2081c7d39f5d657e5722ecf022948
Rob Ingram <rob@headcrash.org>
2018-11-27 14:11:15 +1300

    Add test for daily recursion

--- tests/pagetypes/EventsOverviewPageTest.php
+++ tests/pagetypes/EventsOverviewPageTest.php
@@ -11,6 +11,16 @@ class EventsOverviewPageTest extends SapphireTest
 {
     protected static $fixture_file = 'fixtures/EventsOverviewPageTest.yml';
 
+    public function testShouldReturnDailyRecurringDates()
+    {
+        $event = $this->objFromFixture(EventsPage::class, 'daily');
+        $event->copyVersionToStage(Versioned::DRAFT, Versioned::LIVE);
+        $overview = $this->objFromFixture(EventsOverviewPage::class, 'daily_overview');
+        $eventDates = $overview->getEventList('2018-11-01', $end = '2018-12-31');
+        $this->assertEquals(5, count($eventDates));
+        $this->assertEquals('2018-11-07', $eventDates[1]->StartDate);
+    }
+
     public function testShouldReturnWeeklyRecurringDates()
     {
         $event = $this->objFromFixture(EventsPage::class, 'weekly');
@@ -29,6 +39,24 @@ class EventsOverviewPageTest extends SapphireTest
         $eventDates = $overview->getEventList('2018-11-01', $end = '2018-12-31');
         $this->assertEquals(4, count($eventDates));
         $this->assertEquals('2018-11-10', $eventDates[0]->StartDate);
+    }
+
+    public function testShouldExcludeExceptionsForDailyEvents()
+    {
+        $event = $this->objFromFixture(EventsPage::class, 'daily');
+        $exception = $this->objFromFixture(RecurringException::class, 'fordaily1');
+        $exception->EventsPageID = $event->ID;
+        $exception->write();
+        $exception = $this->objFromFixture(RecurringException::class, 'fordaily2');
+        $exception->EventsPageID = $event->ID;
+        $exception->write();
+        $event->copyVersionToStage(Versioned::DRAFT, Versioned::LIVE);
+        $overview = $this->objFromFixture(EventsOverviewPage::class, 'daily_overview');
+        $eventDates = $overview->getEventList('2018-11-01', $end = '2018-12-31');
+        $this->assertEquals(3, count($eventDates));
+        $eventDateStrings = array_map(function($ev) { return $ev->StartDate; }, $eventDates->toArray());
+        $this->assertNotContains('2018-11-13', $eventDateStrings);
+        $this->assertNotContains('2018-11-25', $eventDateStrings);
     }
 
     public function testShouldExcludeExceptionsForWeeklyEvents()
